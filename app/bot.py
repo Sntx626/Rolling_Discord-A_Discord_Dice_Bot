6525fbfa458e756c13033b96d95eff0ba7e5ece7
Sntx626 <syntaxzockt@gmail.com>
2020-09-10 17:17:48 +0200

    preparing bot.py for removal of tools

--- app/bot.py
+++ app/bot.py
@@ -14,6 +14,8 @@ with open("config.json") as f:
     config = json.load(f)
     client_secret = config["client secret"]
     command_prefix = config["command prefix"]
+    name_suffix = config["results suffix"]
+
 import datetime
 
 config["results suffix"] = datetime.datetime.now().strftime("%f")
@@ -27,8 +29,31 @@ if config["logging enabled"]:
 
     with open("logs/results_"+ config["results suffix"] +".json", 'w') as f:
         json.dump(base_results, f, indent=2)
+    
+    with open("logs/results_"+ name_suffix +".json") as f:
+        results = json.load(f)
 
-import tools
+def print_on_command_call(ctx_author, command_name, input):
+    print(f"\n{ctx_author} '{command_prefix}{command_name} {input}':")
+
+def print_bot(output, ctx_author, command_name, input):
+    print(output)
+    if config["logging enabled"]:
+        add_results_entry(f"\n{ctx_author} '{command_prefix}{command_name} {input}':", output)
+
+def add_results_entry(input, output):
+    with open("logs/results_"+name_suffix+".json") as f:
+        data = json.load(f)
+
+    result = {
+    "input":input,
+    "output":output,
+    "timestamp":datetime.datetime.now().strftime("%c")
+    }
+
+    data.append(result)
+    with open("logs/results_"+name_suffix+".json", 'w') as f:
+        json.dump(data, f, indent=2)
 
 import os
 from itertools import cycle
@@ -50,14 +75,6 @@ async def on_ready():
     change_status.start()
     print('\n\tBot is online.')
 
-'''
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send('Invalid command used.')
-        print('Error: Invalid command used.')
-'''
-
 ##### tasks #####
 
 @tasks.loop(seconds=20)
@@ -67,44 +84,44 @@ async def change_status():
 ##### commands #####
 
 @client.command()
-@commands.is_owner()
+@commands.has_permissions(administrator=True)
 async def shutdown(ctx):
-    tools.print_on_command_call(ctx.author, 'shutdown', '')
+    print_on_command_call(ctx.author, 'shutdown', '')
     await ctx.bot.logout()
-    tools.print_bot(f"shutdown...")
+    print_bot(f"shutdown...")
 
 # load cogs
 @client.command()
 @commands.has_permissions(administrator=True)
 async def load(ctx, extension):
-    tools.print_on_command_call(ctx.author, 'load', extension)
+    print_on_command_call(ctx.author, 'load', extension)
     client.load_extension(f'cogs.{extension}')
     embed = discord.Embed(
         description = f'loaded: {extension}',
         colour = discord.Colour.blue()
     )
     await ctx.send(embed=embed)
-    tools.print_bot(f"loaded: '{extension}'")
+    print_bot(f"loaded: '{extension}'")
 
 
 # unload cogs
 @client.command()
 @commands.has_permissions(administrator=True)
 async def unload(ctx, extension):
-    tools.print_on_command_call(ctx.author, 'unload', extension)
+    print_on_command_call(ctx.author, 'unload', extension)
     client.unload_extension(f'cogs.{extension}')
     embed = discord.Embed(
         description = f'unloaded: {extension}',
         colour = discord.Colour.blue()
     )
     await ctx.send(embed=embed)
-    tools.print_bot(f"unloaded: '{extension}'")
+    print_bot(f"unloaded: '{extension}'")
 
 # reload cogs
 @client.command()
 @commands.has_permissions(administrator=True)
 async def reload(ctx, *, extension=''):
-    tools.print_on_command_call(ctx.author, 'reload', extension)
+    print_on_command_call(ctx.author, 'reload', extension)
     if extension == '':
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py') and not filename[:-3].endswith('.py'):
@@ -115,7 +132,7 @@ async def reload(ctx, *, extension=''):
             colour = discord.Colour.blue()
         )
         await ctx.send(embed=embed)
-        tools.print_bot(f"reloaded: all", ctx.author, "reload", extension)
+        print_bot(f"reloaded: all", ctx.author, "reload", extension)
     else:
         client.unload_extension(f'cogs.{extension}')
         client.load_extension(f'cogs.{extension}')
@@ -124,7 +141,7 @@ async def reload(ctx, *, extension=''):
             colour = discord.Colour.blue()
         )
         await ctx.send(embed=embed)
-        tools.print_bot(f"reloaded: '{extension}'")
+        print_bot(f"reloaded: '{extension}'")
 
 ##### finalize and run #####
 
